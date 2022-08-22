b158ecff52f5980a71154fc9b540e1f21c90847d
AnaDaniela96 <108939830+AnaDaniela96@users.noreply.github.com>
2022-08-22 12:40:31 -0500

    Versión con historia 1

--- src/main.js
+++ src/main.js
@@ -1,16 +1,14 @@
 import data from './data/harrypotter/data.js';//data de Harry Potter
  const root = document.getElementById('root')
+ root.classList = 'harry-style'
 
 let personajes = data.characters
-console.log(personajes);
-
-
 
 const generadorHTML = (characters)=>{
     const div = document.createElement('div')
-    div.classList = 'harry-style'
+    div.classList= 'divStyle'
 
-    let titleName = document.createElement('h2') 
+    let titleName = document.createElement('h4') 
     titleName.textContent=characters.name
 
    div.append(titleName)
